83ac809113b1cebc9c5ec38dfd380646d54999e0
LeonFBeymar <beymarleonf18@gmail.com>
2022-05-21 18:44:21 -0300

    Intentando terminar el metodo Simulaciones

--- tpbolillero.core/Simulacion.cs
+++ tpbolillero.core/Simulacion.cs
@@ -13,21 +13,33 @@ namespace tpbolillero.core
 
         
 
-        public long simularConHilos(Bolillero bolillero, List<byte> jugada, long cantidad, long cantidadHilos)
+        public long simularConHilos(Bolillero bolillero, List<byte> jugada, long cantidadSimulaciones, long cantidadHilos)
         {
+            long SimulacionesPorHilo = cantidadSimulaciones / cantidadHilos;
+
             Task<long>[] tareas = new Task<long>[cantidadHilos];
-            for (int i = 0; i < cantidad; i++)
+           
+            for (long i = 0; i < cantidadHilos; i++)
             {
-                Bolillero bolilleroClon = (Bolillero)bolillero.Clone(); 
-                tareas[i] = Task<long>.Run(()=> simularSinHilos(bolilleroClon, jugada, cantidad));
-                var a = cantidad / cantidadHilos;
-
-
-                Task<long>.WaitAll(tareas);
                 
-                return tareas.Sum();
+                for (int a = 0; a < SimulacionesPorHilo; a++)
+                {
+                    Bolillero bolilleroClon = (Bolillero)bolillero.Clone();
+                    tareas[i] = Task<long>.Run(()=> simularSinHilos(bolilleroClon, jugada, cantidadSimulaciones));
+                }
+                
             }
+            Task<long>.WaitAll(tareas);
             
         }
     }
 }
+
+
+// sumadeGanadas = sumadeGanadas + tareas.Sum(x => i);
+//CLonamos el bolillero y se lo asignamos  a un objeto de tipo Bolillero return sumadeGanadas;  long sumadeGanadas = 0;
+
+//                     Bolillero bolilleroClon = (Bolillero)bolillero.Clone();
+//                     //Asignamos a cada tarea su propia simulacion con su propio bolillero
+//                     tareas[i] = Task<long>.Run(()=> simularSinHilos(bolilleroClon, jugada, cantidadSimulaciones));
+//                     //La coleccion tareas en el idice N ya tiene la cantidad de veces que salieron verdaderas las jugadas.
